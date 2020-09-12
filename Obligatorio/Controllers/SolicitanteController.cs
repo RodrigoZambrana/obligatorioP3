35e108af6ef360627f14dd4b2404229bc4da7564
RodrigoZambrana <rodrigo@lacnic.net>
2020-09-12 19:17:12 -0300

    agrego soliicitante

---
+++ Obligatorio/Controllers/SolicitanteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Obligatorio.Controllers
+{
+    public class SolicitanteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public ActionResult PresentarProyecto()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
