973591babde01ace8ec99f3c3e08a242e3505072
RodrigoZambrana <sergiorodrigo2611@gmail.com>
2020-09-14 01:53:05 +0200

    Conexion con base de datos y creacion de Repositorios

---
+++ Repositorios/RepositorioProyectos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Repositorios
+{
+    class RepositorioProyectos
+    {
+        public bool Add(Proyecto unProyecto)
+        {
+            //Tendría todo el código de ADO.NET para hacer el INSERT  a través de comandos.
+            return true;
+        }
+        public bool Remove(Proyecto unProyecto)
+        {
+            return true;
+        }
+        public bool Remove(int Id)
+        {
+            return true;
+        }
+        public bool Update(Proyecto unProyecto)
+        {
+            //No se modifica la clave primaria, o identificador
+            return true;
+        }
+
+        public Proyecto FindById(int id)
+        {
+            //retorna la categoría con ese id
+            return null;
+        }
+        public List<Proyecto> FindAll()
+        {
+            return null;
+        }
+        public List<Proyecto> FindXPalabraEnNombre(string nom)
+        {
+            //hay que hacer la búsqueda por nombre en las categorías.
+            throw new NotImplementedException("Ups, te falta implementar este método");
+            //return null;
+        }
+    }
+}
---
+++ Repositorios/RepositorioUsuarios.cs
@@ -0,0 +1,48 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    public class RepositorioUsuarios
+    {
+        public bool Add(Usuario unUsuario)
+        {
+            //Tendría todo el código de ADO.NET para hacer el INSERT  a través de comandos.
+            return true;
+        }
+        public bool Remove(Usuario unUsuario)
+        {
+            return true;
+        }
+        public bool Remove(int Id)
+        {
+            return true;
+        }
+        public bool Update(Usuario unUsuario)
+        {
+            //No se modifica la clave primaria, o identificador
+            return true;
+        }
+
+        public Usuario FindById(int id)
+        {
+            //retorna la categoría con ese id
+            return null;
+        }
+        public List<Usuario> FindAll()
+        {
+            return null;
+        }
+        public List<Usuario> FindXPalabraEnNombre(string nom)
+        {
+            //hay que hacer la búsqueda por nombre en las categorías.
+            throw new NotImplementedException("Ups, te falta implementar este método");
+            //return null;
+        }
+
+    }
+}
